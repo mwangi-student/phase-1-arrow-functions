21ac1506e5ec63c92c1e39904816b7acba2affdc
chieby-mwangi <chiebyrah@gmail.com>
2024-09-29 13:12:11 +0300

    phase-1-arrow-functions

---
+++ index.js
@@ -0,0 +1,10 @@
+// Define the divide function using a function expression
+const divide = function () {
+  return 2000 / 100;
+};
+
+// Define the square function using arrow syntax
+const square = (x) => x * x;
+
+// Define the add function using arrow syntax
+const add = (a, b) => a + b;
